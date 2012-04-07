06a46a356d51cdd1b4fc6c4f69373e21309fee0c
Don Rowe <rowedonalde@gmail.com>
2012-04-06 23:12:14 -0700

    Removed old draw instructions from hello-sphere.js and replaced them with the solid sphere

--- homework/pipeline/hello-sphere.js
+++ homework/pipeline/hello-sphere.js
@@ -49,51 +49,17 @@
 
     // Build the objects to display.
     objectsToDraw = [
-        {
-            color: { r: 1.0, g: 0, b: 0 },
-            vertices: [].concat(
-                [ 0.0, 0.0, 0.0 ],
-                [ 0.5, 0.0, -0.75 ],
-                [ 0.0, 0.5, 0.0 ]
-            ),
-            mode: gl.TRIANGLES
-        },
 
-        {
-            color: { r: 0.0, g: 1.0, b: 0 },
-            vertices: [].concat(
-                [ 0.25, 0.0, -0.5 ],
-                [ 0.75, 0.0, -0.5 ],
-                [ 0.25, 0.5, -0.5 ]
-            ),
-            mode: gl.TRIANGLES
-        },
-
-        {
-            color: { r: 0.0, g: 0.0, b: 1.0 },
-            vertices: [].concat(
-                [ -0.25, 0.0, 0.5 ],
-                [ 0.5, 0.0, 0.5 ],
-                [ -0.25, 0.5, 0.5 ]
-            ),
-            mode: gl.TRIANGLES
-        },
-
-        {
-            color: { r: 0, g: 0, b: 1.0 },
-            vertices: [].concat(
-                [ -1.0, -1.0, 0.75 ],
-                [ -1.0, -0.1, -1.0 ],
-                [ -0.1, -0.1, -1.0 ],
-                [ -0.1, -1.0, 0.75 ]
-            ),
-            mode: gl.LINE_LOOP
-        },
-
-        {
+        /*{
             color: { r: 0, g: 0.5, b: 0 },
             vertices: Shapes.toRawLineArray(Shapes.icosahedron()),
             mode: gl.LINES
+        },*/
+        
+        {
+            color: { r: 1, g: 0, b: 0.5 },
+            vertices: Shapes.sphere()['strip'],
+            mode: gl.TRIANGLE_STRIP
         }
     ];
 
