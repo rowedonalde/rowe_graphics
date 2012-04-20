7f55848bbe2700982d27ebb916e181bcc69c6163
Don Rowe <rowedonalde@gmail.com>
2012-04-20 15:05:54 -0700

    This version of hello-webgl-again-sphere.js is now correctly a fork of hello-webgl-again.js

--- homework/pipeline/hello-webgl-again-sphere.js
+++ homework/pipeline/hello-webgl-again-sphere.js
@@ -19,7 +19,11 @@
         abort = false,
 
         // Important state variables.
+        currentRotation = 0.0,
+        currentInterval,
+        rotationMatrix,
         vertexPosition,
+        vertexColor,
 
         // An individual "draw object" function.
         drawObject,
@@ -29,7 +33,78 @@
 
         // Reusable loop variables.
         i,
-        max;
+        maxi,
+        j,
+        maxj,
+
+        /*
+         * This code does not really belong here: it should live
+         * in a separate library of matrix and transformation
+         * functions.  It is here only to show you how matrices
+         * can be used with GLSL.
+         *
+         * Based on the original glRotate reference:
+         *     http://www.opengl.org/sdk/docs/man/xhtml/glRotate.xml
+         */
+        getRotationMatrix = function (angle, x, y, z) {
+            // In production code, this function should be associated
+            // with a matrix object with associated functions.
+            var axisLength = Math.sqrt((x * x) + (y * y) + (z * z)),
+                s = Math.sin(angle * Math.PI / 180.0),
+                c = Math.cos(angle * Math.PI / 180.0),
+                oneMinusC = 1.0 - c,
+
+                // We can't calculate this until we have normalized
+                // the axis vector of rotation.
+                x2, // "2" for "squared."
+                y2,
+                z2,
+                xy,
+                yz,
+                xz,
+                xs,
+                ys,
+                zs;
+
+            // Normalize the axis vector of rotation.
+            x /= axisLength;
+            y /= axisLength;
+            z /= axisLength;
+
+            // *Now* we can calculate the other terms.
+            x2 = x * x;
+            y2 = y * y;
+            z2 = z * z;
+            xy = x * y;
+            yz = y * z;
+            xz = x * z;
+            xs = x * s;
+            ys = y * s;
+            zs = z * s;
+
+            // GL expects its matrices in column major order.
+            return [
+                (x2 * oneMinusC) + c,
+                (xy * oneMinusC) + zs,
+                (xz * oneMinusC) - ys,
+                0.0,
+
+                (xy * oneMinusC) - zs,
+                (y2 * oneMinusC) + c,
+                (yz * oneMinusC) + xs,
+                0.0,
+
+                (xz * oneMinusC) + ys,
+                (yz * oneMinusC) - xs,
+                (z2 * oneMinusC) + c,
+                0.0,
+
+                0.0,
+                0.0,
+                0.0,
+                1.0
+            ];
+        };
 
     // Grab the WebGL rendering context.
     gl = GLSLUtilities.getGL(canvas);
@@ -77,9 +152,25 @@
     ];
 
     // Pass the vertices to WebGL.
-    for (i = 0, max = objectsToDraw.length; i < max; i += 1) {
+    for (i = 0, maxi = objectsToDraw.length; i < maxi; i += 1) {
         objectsToDraw[i].buffer = GLSLUtilities.initVertexBuffer(gl,
                 objectsToDraw[i].vertices);
+
+        if (!objectsToDraw[i].colors) {
+            // If we have a single color, we expand that into an array
+            // of the same color over and over.
+            objectsToDraw[i].colors = [];
+            for (j = 0, maxj = objectsToDraw[i].vertices.length / 3;
+                    j < maxj; j += 1) {
+                objectsToDraw[i].colors = objectsToDraw[i].colors.concat(
+                    objectsToDraw[i].color.r,
+                    objectsToDraw[i].color.g,
+                    objectsToDraw[i].color.b
+                );
+            }
+        }
+        objectsToDraw[i].colorBuffer = GLSLUtilities.initVertexBuffer(gl,
+                objectsToDraw[i].colors);
     }
 
     // Initialize the shaders.
@@ -114,13 +205,19 @@
     // Hold on to the important variables within the shaders.
     vertexPosition = gl.getAttribLocation(shaderProgram, "vertexPosition");
     gl.enableVertexAttribArray(vertexPosition);
+    vertexColor = gl.getAttribLocation(shaderProgram, "vertexColor");
+    gl.enableVertexAttribArray(vertexColor);
+    rotationMatrix = gl.getUniformLocation(shaderProgram, "rotationMatrix");
 
     /*
      * Displays an individual object.
      */
     drawObject = function (object) {
-        gl.uniform3f(gl.getUniformLocation(shaderProgram, "color"),
-            object.color.r, object.color.g, object.color.b);
+        // Set the varying colors.
+        gl.bindBuffer(gl.ARRAY_BUFFER, object.colorBuffer);
+        gl.vertexAttribPointer(vertexColor, 3, gl.FLOAT, false, 0, 0);
+
+        // Set the varying vertex coordinates.
         gl.bindBuffer(gl.ARRAY_BUFFER, object.buffer);
         gl.vertexAttribPointer(vertexPosition, 3, gl.FLOAT, false, 0, 0);
         gl.drawArrays(object.mode, 0, object.vertices.length / 3);
@@ -133,8 +230,11 @@
         // Clear the display.
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
+        // Set up the rotation matrix.
+        gl.uniformMatrix4fv(rotationMatrix, gl.FALSE, new Float32Array(getRotationMatrix(currentRotation, 0, 1, 0)));
+
         // Display the objects.
-        for (i = 0, max = objectsToDraw.length; i < max; i += 1) {
+        for (i = 0, maxi = objectsToDraw.length; i < maxi; i += 1) {
             drawObject(objectsToDraw[i]);
         }
 
@@ -142,7 +242,23 @@
         gl.flush();
     };
 
-    // ...and finally, do the initial display.
+    // Draw the initial scene.
     drawScene();
+
+    // Set up the rotation toggle: clicking on the canvas does it.
+    $(canvas).click(function () {
+        if (currentInterval) {
+            clearInterval(currentInterval);
+            currentInterval = null;
+        } else {
+            currentInterval = setInterval(function () {
+                currentRotation += 1.0;
+                drawScene();
+                if (currentRotation >= 360.0) {
+                    currentRotation -= 360.0;
+                }
+            }, 30);
+        }
+    });
 
 }(document.getElementById("hello-webgl")));
